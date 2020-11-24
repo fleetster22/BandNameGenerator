f346675e628ccb121ba53841e7292edb25c972f0
fleetster22 <fleetster22@gmail.com>
2020-11-24 10:58:20 -0500

    Day 1 Band Name Generator using string and variable concatenation

---
+++ main.py
@@ -0,0 +1,28 @@
+# 100 Days of Code by Angela Yu- Udemy Course
+# Day 1
+# print('Day 1- String Manipulation\nNew lines can be created with a backslash and n.')
+
+# Functions inside of functions
+# input("What is your name?")
+# print("Hello " + input("What is your name? "))
+# print("Your name has " + str((len(input("What is your first name? ")))) + " characters.")
+
+# Variables
+# user_name = input("What is your name?")
+# age = 47
+# print("Hello " + name + ". You are " + str(age) + " years old.")
+
+# switch the values of the variables a and b
+# a = input("a:")
+# b = input("b:")
+# c = a
+# a = b
+# b = c
+# print("a = " + a)
+# print("b = " + b)
+
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+print("Also, I now have some ideas of what your passwords and security question answers are.")
